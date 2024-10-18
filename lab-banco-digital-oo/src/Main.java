de371ddcd7f6c6451a1e398b7dbab2324ee01ad2
Gabriell Henneg <134616195+GabriellHenneg@users.noreply.github.com>
2024-10-18 20:48:37 -0300

    Add files via upload

---
+++ lab-banco-digital-oo/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente venilton = new Cliente();
+		venilton.setNome("Venilton");
+		
+		Conta cc = new ContaCorrente(venilton);
+		Conta poupanca = new ContaPoupanca(venilton);
+		Conta digital = new ContaDigital(venilton);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+
+		digital.depositar(2500);
+		digital.imprimirExtrato();
+	
+	}
+
+}
